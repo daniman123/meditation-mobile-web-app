a2e69c7c23b34b08a429ea71d22e821ee553e8ca
daniman123 <daniel-saleh@hotmail.com>
2025-04-10 18:01:15 +0200

    build: implement prettier and configure eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,19 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          args: "after-used",
+          caughtErrors: "all",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
