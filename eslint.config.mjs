1ba0fdf2849c8f4643deaf18871d36ac9afc814a
daniman123 <daniel-saleh@hotmail.com>
2025-04-10 20:33:22 +0200

    feat: implement storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,11 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:storybook/recommended"
+  ),
   {
     rules: {
       "no-unused-vars": [
@@ -22,6 +26,15 @@ const eslintConfig = [
           argsIgnorePattern: "^_",
         },
       ],
+    },
+  },
+  {
+    files: ["**/*.stories.@(ts|tsx|js|jsx|mjs|cjs)"],
+    rules: {
+      // example of overriding a rule
+      "storybook/hierarchy-separator": "error",
+      // example of disabling a rule
+      "storybook/default-exports": "off",
     },
   },
 ];
