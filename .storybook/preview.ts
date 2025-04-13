fefecab34ccc6f00f3e134fa76cdfd3654530447
daniman123 <daniel-saleh@hotmail.com>
2025-04-13 18:55:07 +0200

    build: implement extended viewports

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,13 +1,19 @@
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 import type { Preview } from "@storybook/react";
+import "../src/app/globals.css";
 
 const preview: Preview = {
   parameters: {
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
     },
+    layout: "fullscreen",
   },
 };
 
