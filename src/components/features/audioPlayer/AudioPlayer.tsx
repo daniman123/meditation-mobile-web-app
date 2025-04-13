6b828208d03d5d4f86f9c5fdf816f37c12003467
daniman123 <daniel-saleh@hotmail.com>
2025-04-13 20:38:01 +0200

    revert: cleared audioplayer component

--- src/components/features/audioPlayer/AudioPlayer.tsx
+++ src/components/features/audioPlayer/AudioPlayer.tsx
@@ -1,26 +1,11 @@
 import { IAudioData } from "@/lib/audioMetaData/types";
-import { useRef } from "react";
 
 export interface IAudioPlayer {
   audioData: IAudioData;
 }
 
 const AudioPlayer = ({ audioData }: IAudioPlayer) => {
-  const audioRef = useRef<null | HTMLAudioElement>(null);
-
-  return (
-    <div className="h-21 w-3">
-      <div>
-        asdd
-        <audio ref={audioRef} controls>
-          <source
-            src={`/audio/classic/${audioData.fileName}`}
-            type="audio/mpeg"
-          />
-        </audio>
-      </div>
-    </div>
-  );
+  return <div className="">{audioData.fileName}</div>;
 };
 
 export default AudioPlayer;
